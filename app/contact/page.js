c5b0551d4c5db02125e49101041bec0d91b1e7bd
Bhavesh Yadav <codezera3@gmail.com>
2023-07-14 13:08:26 +0530

    changed url in env files

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,8 +3,6 @@ import React from 'react';
 
 const contact = () => {
 
-
-
   return (
     <div className='container bg_gradient max-w-[1470px] flex flex-col items-center'>
       <div className='w-full mt-10'>
--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
     console.log("Building connection.......")
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(`${process.env.MONGO_URL}`)
     } catch (error) {
         console.log(error.message)
     }

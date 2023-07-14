cb932cef606812a54a719e2de28f1a6a0ea9c838
Bhavesh Yadav <codezera3@gmail.com>
2023-07-14 12:48:46 +0530

    first trial

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -17,7 +17,7 @@ const ContactCard = () => {
 
 
     try {
-        const response = await fetch('/api/contact', {
+        const response = await fetch('http://localhost:3000/api/contact', {
             method: "POST",
             headers: {"Content_Type": "application/json"},
             body: JSON.stringify({ name, email, phone, message })
